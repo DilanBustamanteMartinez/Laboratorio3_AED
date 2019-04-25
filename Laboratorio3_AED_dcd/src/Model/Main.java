71b52d9678a8f6661835020d37771ae399800269
DanielaOtero17 <jessot@hotmail.com>
2019-04-24 22:10:59 -0500

    Actualization

--- Laboratorio3_AED_dcd/src/Model/Main.java
+++ Laboratorio3_AED_dcd/src/Model/Main.java
@@ -32,18 +32,19 @@ public void modifyMarket() {
 	
 	public void addFile(){
 		
+		
+	}	
+	
+	public File selectFile() {
+		
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
 		int result = fileChooser.showOpenDialog(this);
 		if (result == JFileChooser.APPROVE_OPTION) {
 		    File selectedFile = fileChooser.getSelectedFile();
 		    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+		    selected = selectedFile;
 		}
-	}	
-	
-	public File selectFile() {
-		
-		
 		return selected;
 		
 	}
